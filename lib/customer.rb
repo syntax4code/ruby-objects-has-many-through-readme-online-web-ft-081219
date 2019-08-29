b2920ab50ef630eaf4f1603fb7b76822183cc423
Warren Harper II <warrenharper2@att.net>
2019-08-29 20:29:36 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,23 @@
 class Customer
-  
+  attr_accessor :name, :age
+
+    @@all = []
+
+    def initialize(name, age)
+      @name = name
+      @age = age
+      @@all << self
+    end
+
+    def self.all
+      @@all
+    end
 end
+
+sam = Customer.new("Sam", 27)
+  pat = Waiter.new("Pat", 2)
+  alex = Waiter.new("Alex", 5)
+
+  sam.new_meal(pat, 50, 10) # A Customer creates a Meal, passing in a Waiter instance
+  sam.new_meal(alex, 20, 3) # A Customer creates a Meal, passing in a Waiter instance
+  pat.new_meal(sam, 30, 5) # A Waiter creates a Meal, passing in a Customer instance
